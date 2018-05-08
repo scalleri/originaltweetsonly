f7540f2c4a6b429aa455aeb01e19a400df00ea0e
scalleri <selenacalleri@icloud.com>
2018-05-08 10:04:44 +0200

    updated batchslicer

--- tweet_dumper.py
+++ tweet_dumper.py
@@ -7,12 +7,13 @@ import tweepy #https://github.com/tweepy/tweepy
 import csv
 import sys
 import os
+import config
 
 #Twitter API credentials
-consumer_key = 'Pv7Zwnl1ONro0FuudHHkoNO4Y'
-consumer_secret = "nRT4TcaGYBW3UShKLlftpKid0MdDowvyRgbSRc8BvkYuVb2Vb5"
-access_key = "900667241947070469-0t0xORmdUsfI5SPTaZqfFkREa1Vrh4M"
-access_secret = "Atjn9HMxWKdmrMXReMzenjyDD8GUdmlH6Mnj4eOvzxjIE"
+consumer_key = config.consumer_key
+consumer_secret = config.consumer_secret
+access_key = config.access_key
+access_secret = config.access_secret
 
 def main(user_list):
 	user_names = open(user_list)
