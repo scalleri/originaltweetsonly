67d097e11c7d03cd440c4dc04c3acf741a75c757
scalleri <selenacalleri@icloud.com>
2018-05-08 10:09:48 +0200

    updated batchslicer

--- config.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'Pv7Zwnl1ONro0FuudHHkoNO4Y'
-consumer_secret = "nRT4TcaGYBW3UShKLlftpKid0MdDowvyRgbSRc8BvkYuVb2Vb5"
-access_key = "900667241947070469-0t0xORmdUsfI5SPTaZqfFkREa1Vrh4M"
-access_secret = "Atjn9HMxWKdmrMXReMzenjyDD8GUdmlH6Mnj4eOvzxjIE"
