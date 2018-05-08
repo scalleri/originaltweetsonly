88d911f9ff633c91a0663c9ddcca1d1c759b553a
scalleri <selenacalleri@icloud.com>
2018-05-08 10:01:34 +0200

    updated batchslicer

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_secret = ''
+consumer_key = 'Pv7Zwnl1ONro0FuudHHkoNO4Y'
+consumer_secret = "nRT4TcaGYBW3UShKLlftpKid0MdDowvyRgbSRc8BvkYuVb2Vb5"
+access_key = "900667241947070469-0t0xORmdUsfI5SPTaZqfFkREa1Vrh4M"
+access_secret = "Atjn9HMxWKdmrMXReMzenjyDD8GUdmlH6Mnj4eOvzxjIE"
